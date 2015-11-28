8f67b0e12123709790a7eb34b6e0f6ffea65564b
Nick P Smith <npsmith1990@gmail.com>
2015-11-27 21:28:32 -0700

    Initial Commit

---
+++ MyLittleMonster/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyLittleMonster
+//
+//  Created by Nicholas Smith on 11/27/15.
+//  Copyright © 2015 Nicholas Smith. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
